0ae748600902ba8a22003fc330e5689d57c3cedd
Black Wyvern Crypto <80482169+black-wyvern-dev@users.noreply.github.com>
2022-06-20 00:23:45 +0900

    delete unnecessary files

--- src/components/config.tsx
+++ src/components/config.tsx
@@ -8,11 +8,6 @@ export const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
     'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
 );
 
-export const MAGIC_EDEN_PROGRAM_PUBKEY = new PublicKey('MEisE1HzehtrDpAAT8PnLHjpSSkRYakotTuJRPjTpo8');
-export const SOLANART_PROGRAM_PUBKEY = new PublicKey('CJsLwbP1iu5DuUikHEJnLfANgKy6stB2uFgvBBHoyxwz');
-export const DIGITALEYES_PROGRAM_PUBKEY = new PublicKey('A7p8451ktDCHq5yYaHczeLMYsjRsAkzc3hCXcSrwYHU7');
-export const DIGITALEYES_DIRECTSELL_PROGRAM_PUBKEY = new PublicKey('7t8zVJtPCFAqog1DcnB6Ku1AVKtWfHkCiPi1cAvcJyVF');
-export const SOLSEA_PROGRAM_PUBKEY = new PublicKey('617jbWo616ggkDxvW1Le8pV38XLbVSyWY8ae6QUmGBAU');
 export const EXCHANGE_PROGRAM_PUBKEY = new PublicKey('AmK5g2XcyptVLCFESBCJqoSfwV3znGoVYQnqEnaAZKWn');
 export const MEMO_V2_PROGRAM_PUBKEY = new PublicKey('MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr');
 
